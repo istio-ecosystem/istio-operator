ba2b58fe23dd6670419f71b067dd0d47b615b9aa
Martin Ostrowski <mostrowski@google.com>
2019-06-24 10:42:07 -0700

    Copyrights

--- pkg/util/path.go
+++ pkg/util/path.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package util
 
 import (
@@ -73,7 +87,7 @@ func IsVPathElement(pe string) bool {
 	return len(pe) > 0
 }
 
-// PathKV the key and value string parts of the entire key/value path element.
+// Path KVreturns the key and value string parts of the entire key/value path element.
 // It returns an error if pe is not a key/value path element.
 func PathKV(pe string) (k, v string, err error) {
 	if !IsKVPathElement(pe) {
