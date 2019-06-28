9512846894cb4e1e2db3d699c157218df5e5025f
Martin Ostrowski <mostrowski@google.com>
2019-06-28 13:10:53 -0700

    Lint

--- pkg/component/feature/feature.go
+++ pkg/component/feature/feature.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package feature
 
 import (
@@ -208,8 +222,8 @@ func (f *AutoInjectionFeature) RenderManifest() (string, util.Errors) {
 }
 
 // newComponentOptions creates a component.ComponentOptions ptr from the given parameters.
-func newComponentOptions(cff *CommonFeatureFields, featureName name.FeatureName) *component.ComponentOptions {
-	return &component.ComponentOptions{
+func newComponentOptions(cff *CommonFeatureFields, featureName name.FeatureName) *component.Options {
+	return &component.Options{
 		InstallSpec: cff.InstallSpec,
 		FeatureName: featureName,
 		Translator:  cff.Traslator,
