ccb88278cc6af899f80f663c75fb87c1e6698de8
Martin Ostrowski <mostrowski@google.com>
2019-08-01 17:34:34 -0700

    Move to manifest versions

--- cmd/mesh/profile-list.go
+++ cmd/mesh/profile-list.go
@@ -18,15 +18,6 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
-
-	"istio.io/operator/pkg/httpget"
-	"istio.io/operator/pkg/version"
-	binversion "istio.io/operator/version"
-)
-
-const (
-	versionsMapURL = "https://raw.githubusercontent.com/istio/operator/master/version/versions.yaml"
 )
 
 func profileListCmd(rootArgs *rootArgs) *cobra.Command {
@@ -43,35 +34,5 @@ func profileListCmd(rootArgs *rootArgs) *cobra.Command {
 
 func profileList(args *rootArgs) {
 	checkLogsOrExit(args)
-
-	b, err := httpget.Get(versionsMapURL)
-	if err != nil {
-		logAndFatalf(args, err.Error())
-	}
-	var versions []*version.CompatibilityMapping
-	if err = yaml.Unmarshal(b, &versions); err != nil {
-		logAndFatalf(args, err.Error())
-	}
-
-	var myVersionMap *version.CompatibilityMapping
-	for _, v := range versions {
-		if v.OperatorVersion.Equal(binversion.OperatorBinaryGoVersion) {
-			myVersionMap = v
-		}
-	}
-
-	if myVersionMap == nil {
-		logAndFatalf(args, "This operator version (%s) was not found in the global versions map.", binversion.OperatorBinaryGoVersion.String())
-	}
-
-	fmt.Printf("\nOperator version is %s.\n\n", binversion.OperatorBinaryGoVersion.String())
-	fmt.Println("The following installation package versions are recommended for use with this version of the operator:")
-	for _, v := range myVersionMap.RecommendedIstioVersions {
-		fmt.Printf("  %s\n", v.String())
-	}
-	fmt.Println("\nThe following installation package versions are supported by this version of the operator:")
-	for _, v := range myVersionMap.SupportedIstioVersions {
-		fmt.Printf("  %s\n", v.String())
-	}
-	fmt.Println()
+	fmt.Println("This command is not yet implemented.")
 }
