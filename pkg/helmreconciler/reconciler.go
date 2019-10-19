fd6383f6f23c3863a411b3570659a913f3cc6161
llcao <llcao@cn.ibm.com>
2019-10-18 22:04:57 -0700

    fix tests.

--- pkg/helmreconciler/reconciler.go
+++ pkg/helmreconciler/reconciler.go
@@ -102,13 +102,13 @@ func (h *HelmReconciler) Reconcile() error {
 	}
 
 	// handle the defined callbacks to the generated manifests for each subchart chart.
-	for chartName, manifests := range manifestMap {
-		newManifests, err := h.customizer.Listener().BeginChart(chartName, manifests)
-		if err != nil {
-			return err
-		}
-		manifestMap[chartName] = newManifests
-	}
+	//for chartName, manifests := range manifestMap {
+	//	newManifests, err := h.customizer.Listener().BeginChart(chartName, manifests)
+	//	if err != nil {
+	//		return err
+	//	}
+	//	manifestMap[chartName] = newManifests
+	//}
 	status := h.processRecursive(manifestMap)
 
 	// Delete any resources not in the manifest but managed by operator.
