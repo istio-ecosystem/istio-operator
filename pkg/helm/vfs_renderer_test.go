836bffccfe34156c736d1a78df905fd87a2ac18d
John Howard <howardjohn@google.com>
2019-12-02 08:56:05 -0800

    Fix format

--- pkg/helm/vfs_renderer_test.go
+++ pkg/helm/vfs_renderer_test.go
@@ -17,16 +17,16 @@ package helm
 import "testing"
 
 func TestStripPrefix(t *testing.T) {
-	cases := []struct{
-		in string
+	cases := []struct {
+		in     string
 		prefix string
-		out string
+		out    string
 	}{
 		{"foo/bar", "foo", "bar"},
 		{"a/b/c/d", "a/b/c", "d"},
 		{"a/b/c/d", "a/b", "c/d"},
 	}
-	for _, tt := range cases{
+	for _, tt := range cases {
 		t.Run(tt.in+"~"+tt.prefix, func(t *testing.T) {
 			got := stripPrefix(tt.in, tt.prefix)
 			if got != tt.out {
